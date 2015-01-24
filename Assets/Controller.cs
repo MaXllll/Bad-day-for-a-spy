4ffe9839faba7fcf4204dd9c7a9a819891b0aed9
Gval <goulmy.val@gmail.com>
2015-01-24 16:29:43 +0100

    Commit update des levels

---
+++ Assets/Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed = 7;
+	
+	[SerializeField]
+	private float halfSpeed = 3.5f;
+
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = rigidbody;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		float horizontalSpeed = Input.GetAxis ("Horizontal") * moveSpeed;
+		float verticalSpeed = Input.GetAxis ("Vertical") * moveSpeed;
+		
+		rigid.velocity = new Vector3 (horizontalSpeed, rigid.velocity.y, verticalSpeed);
+
+		if (Input.GetKeyDown (KeyCode.LeftShift)) 
+		{
+			rigid.velocity = Vector3.ClampMagnitude(rigid.velocity, halfSpeed);
+		}
+		
+	}
+}
