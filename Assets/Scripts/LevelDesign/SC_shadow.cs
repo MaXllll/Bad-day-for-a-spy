d4a09b8a0c11aa76640e0cbd1c7557a21476dd65
Fred <marquer.frederic.35@gmail.com>
2015-01-24 20:35:18 +0100

    shadow

---
+++ Assets/Scripts/LevelDesign/SC_shadow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SC_shadow : MonoBehaviour {
+
+	static public bool _b_player_is_in_shadow = false;
+
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		_b_player_is_in_shadow = true;
+	}
+
+	private void OnTriggerExit(Collider collider)
+	{
+		_b_player_is_in_shadow = false;
+	}
+}
