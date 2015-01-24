32d7e221fb1daa2537461b9914dd6aac43fd35c7
Fred <marquer.frederic.35@gmail.com>
2015-01-24 22:05:55 +0100

    visuel pet

---
+++ Assets/Scripts/SC_rotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SC_rotation : MonoBehaviour {
+
+	private Transform _T_gaz;
+	private float f_speed;
+
+	void Start()
+	{
+		_T_gaz = transform;
+		f_speed = Random.value * 40 - 20;
+		_T_gaz.Rotate(0, Random.value * 360, 0, Space.World);
+	}
+	
+	void Update()
+	{
+		_T_gaz.Rotate(0, f_speed * Time.deltaTime, 0, Space.World);
+	}
+}
