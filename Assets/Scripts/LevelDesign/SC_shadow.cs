3d790396c72361691dd653b6f5057e4727643d24
Mathieu Harharidis <hhds.mathieu@gmail.com>
2015-01-25 09:05:16 +0100

    Add Script For Gauge (unfinished)

--- Assets/Scripts/LevelDesign/SC_shadow.cs
+++ Assets/Scripts/LevelDesign/SC_shadow.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class SC_shadow : MonoBehaviour {
 
 	static public bool _b_player_is_in_shadow = false;
-
+	/*
+	[SerializeField]
+	public GameObject go_player;
+	*/
 
 	private void OnTriggerEnter(Collider collider)
 	{
 		_b_player_is_in_shadow = true;
+		//go_player.GetComponents('GaugeManager');
 	}
 
 	private void OnTriggerExit(Collider collider)
---
+++ Assets/Scripts/Player/SC_GaugeManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SC_GaugeManager : MonoBehaviour {
+
+	private int stressValue;
+	private int sartProbability;
+	private int sartLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void EnterIntoShadow(){
+		stressValue = 0;
+	}
+}
