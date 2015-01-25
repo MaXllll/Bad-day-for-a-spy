bd8d6354a88c8cd91bfecfc6023224c4136a8cc6
Gval <goulmy.val@gmail.com>
2015-01-25 10:59:12 +0100

    Commit final de level

---
+++ Assets/SC_Trigger_Teleport.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SC_Trigger_Teleport : MonoBehaviour {
+
+	[SerializeField]
+	GameObject teleport_position;
+
+	[SerializeField]
+	LayerMask player_layer;
+
+	[SerializeField]
+	Transform player_trasform;
+	
+	/*void OnTriggerEnter (Collider col) {
+		if(col. == player_layer.) {
+			player_trasform.position = 
+		}
+	}*/
+}
